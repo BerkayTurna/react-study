3190b2d88682659e10afd4810450b9f07b1deba2
yakreb <berkayturna@gmail.com>
2024-12-24 00:05:23 +0100

    className

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   //use HTML syntax after return in JSX
   // return accepts only one parent
   // curly brackets for JS syntax
+  // classes should called with className
   const userName = "Berkay";
   const users = ["Berkay", "Cansu", "Atlas", "Can"];
 
